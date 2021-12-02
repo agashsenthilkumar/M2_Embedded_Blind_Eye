436d18d041fe2fcef18898abffba6594a63ca630
agashsenthilkumar <94293980+agashsenthilkumar@users.noreply.github.com>
2021-12-02 11:48:34 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,59 +1,66 @@
+#define F_CPU 8000000UL
 #include <avr/io.h>
-#include <MrLcd/MrLCDmega32.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <string.h>
 #include <stdlib.h>
+#include "LCD_16x2_H_file.h"	/* Include LCD header file */
 
-static volatile int pulse = 0;
-static volatile int i = 0;
+#define  Trigger_pin	PA0	/* Trigger pin */
+
+int TimerOverflow = 0;
+
+ISR(TIMER1_OVF_vect)
+{
+	TimerOverflow++;	/* Increment Timer Overflow count */
+}
 
 int main(void)
 {
-  int16_t count_a = 0; 
-  char show_a[16];
-  Initialise();
-  DDRD = 0b11111011;
-  _delay_ms(50);
+	char string[10];
+	long count;
+	double distance;
+	
+	DDRA = 0x01;		/* Make trigger pin as output */
+	PORTD = 0xFF;		/* Turn on Pull-up */
+	
+	LCD_Init();
+	LCD_String_xy(1, 0, "Ultrasonic");
+	
+	sei();			/* Enable global interrupt */
+	TIMSK = (1 << TOIE1);	/* Enable Timer1 overflow interrupts */
+	TCCR1A = 0;		/* Set all bit to zero Normal operation */
 
-  Initialise();
+	while(1)
+	{
+		/* Give 10us trigger pulse on trig. pin to HC-SR04 */
+		PORTA |= (1 << Trigger_pin);
+		_delay_us(10);
+		PORTA &= (~(1 << Trigger_pin));
+		
+		TCNT1 = 0;	/* Clear Timer counter */
+		TCCR1B = 0x41;	/* Capture on rising edge, No prescaler*/
+		TIFR = 1<<ICF1;	/* Clear ICP flag (Input Capture flag) */
+		TIFR = 1<<TOV1;	/* Clear Timer Overflow flag */
 
-  GICR |= 1<<INT0;
-  MCUCR |= 1<<ISC00;
+		/*Calculate width of Echo by Input Capture (ICP) */
+		
+		while ((TIFR & (1 << ICF1)) == 0);/* Wait for rising edge */
+		TCNT1 = 0;	/* Clear Timer counter */
+		TCCR1B = 0x01;	/* Capture on falling edge, No prescaler */
+		TIFR = 1<<ICF1;	/* Clear ICP flag (Input Capture flag) */
+		TIFR = 1<<TOV1;	/* Clear Timer Overflow flag */
+		TimerOverflow = 0;/* Clear Timer overflow count */
 
-  sei();
- 
-  while(1)
-  { 
-    PORTD |= 1<<PIND0;
-    _delay_us(15);
+		while ((TIFR & (1 << ICF1)) == 0);/* Wait for falling edge */
+		count = ICR1 + (65535 * TimerOverflow);	/* Take count */
+		/* 8MHz Timer freq, sound speed =343 m/s */
+		distance = (double)count / 466.47;
 
-    PORTD &= ~(1<<PIND0);
-    count_a = pulse/58;
-
-    Send_A_String("Distance Sensor");
-    GoToMrLCDLocation(1,2);
-    Send_A_String("Distance=");
-    itoa(count_a,show_a,10);
-    Send_A_String(show_a);
-    Send_A_String(" ");
-    GoToMrLCDLocation(13,2);
-    Send_A_String("cm");
-    GoToMrLCDLocation(1,1);
-  }
-}
-
-ISR(INT0_vect)
-{
-  if(i == 0)
-  {
-    TCCR1B |= 1<<CS10;
-    i = 1;
-  }
-  else
-  {
-    TCCR1B = 0;
-    pulse = TCNT1;
-    TCNT1 = 0;
-    i = 0;
-  }
+		dtostrf(distance, 2, 2, string);/* distance to string */
+		strcat(string, " cm   ");	/* Concat unit i.e.cm */
+		LCD_String_xy(2, 0, "Dist = ");
+		LCD_String_xy(2, 7, string);	/* Print distance */
+		_delay_ms(200);
+	}
 }
