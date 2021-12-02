51c178a5c0f74c0f70cdd053cb5465d2bfe7b95e
agashsenthilkumar <94293980+agashsenthilkumar@users.noreply.github.com>
2021-12-02 18:28:19 +0530

    Update and rename project_conf.h to ultra.h

--- 3_Implementation/inc/project_conf.h
+++
@@ -1,5 +0,0 @@
-#include <avr/io.h>
-#include <MrLcd/MrLCDmega32.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-#include <stdlib.h>
---
+++ 3_Implementation/inc/ultra.h
@@ -0,0 +1,37 @@
+#ifndef __ULTRA_H_
+#define __ULTRA_H_
+/**
+ * @file ultra.h
+ * @author Sindhu ()
+ * @brief Project to Ultrasonic sensor to calculate distance ATMega328 P
+ * @version 0.1
+ * @date 2021-11-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * Macro Definitions
+ */
+#define trigPin 2             /**< Trig Pin for ultrasonic sensor */
+
+#define echoPin 3	     /**< Echo Pin for ultrasonic sensor */
+
+
+/**
+ * Include files
+ */ 
+#include <avr/io.h>
+
+/**
+ * Function Definitions
+ */
+
+/**
+
+ * @param state Pin level to which the Ultrsonic sensor Pin should be set
+ */
+
+
+#endif /** __ULTRA_H_ */
