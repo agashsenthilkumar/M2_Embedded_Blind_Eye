6163829f6dde757ffa3fca96a3c29bbe02aaa979
agashsenthilkumar <94293980+agashsenthilkumar@users.noreply.github.com>
2021-12-02 12:45:57 +0530

    Create ultra.c

---
+++ 3_Implementation/src/ultra.c
@@ -0,0 +1,18 @@
+#include "ultra.h"
+#include "LiquidCrystal.h" //Please replace the single quote characters ('') with the parenthesis character (<>)
+
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7); // Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7)
+
+long duration;
+int distanceCm, distanceInch;
+
+
+
+
+void setup() {
+  
+lcd.begin(16,2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+
+}
