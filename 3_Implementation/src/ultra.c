29b81d3d91c8881de8ccb16d51caa8fa1e01055c
agashsenthilkumar <94293980+agashsenthilkumar@users.noreply.github.com>
2021-12-02 15:21:20 +0530

    Update ultra.c

--- 3_Implementation/src/ultra.c
+++ 3_Implementation/src/ultra.c
@@ -1,18 +1,45 @@
 #include "ultra.h"
 #include "LiquidCrystal.h" //Please replace the single quote characters ('') with the parenthesis character (<>)
 
-LiquidCrystal lcd(1, 2, 4, 5, 6, 7); // Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7)
-
-long duration;
-int distanceCm, distanceInch;
-
-
-
-
-void setup() {
-  
-lcd.begin(16,2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
-pinMode(trigPin, OUTPUT);
-pinMode(echoPin, INPUT);
-
+#define trigger 18
+#define echo 19
+ 
+LiquidCrystal lcd(2,3,4,5,6,7);
+ 
+float time=0,distance=0;
+ 
+void setup()
+{
+ lcd.begin(16,2);
+ pinMode(trigger,OUTPUT);
+ pinMode(echo,INPUT);
+ lcd.print(" Ultra sonic");
+ lcd.setCursor(0,1);
+ lcd.print("Distance Meter");
+ delay(2000);
+ lcd.clear();
+ lcd.print(" Circuit Digest");
+ delay(2000);
+}
+ 
+void loop()
+{
+ lcd.clear();
+ digitalWrite(trigger,LOW);
+ delayMicroseconds(2);
+ digitalWrite(trigger,HIGH);
+ delayMicroseconds(10);
+ digitalWrite(trigger,LOW);
+ delayMicroseconds(2);
+ time=pulseIn(echo,HIGH);
+ distance=time*340/20000;
+ lcd.clear();
+ lcd.print("Distance:");
+ lcd.print(distance);
+ lcd.print("cm");
+ lcd.setCursor(0,1);
+ lcd.print("Distance:");
+ lcd.print(distance/100);
+ lcd.print("m");
+ delay(1000);
 }
